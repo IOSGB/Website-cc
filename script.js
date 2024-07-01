b104e362dbb714b5f20ae1eea83f9f68ee62bb45
IOSGB <ioscrackgb@gmail.com>
2024-07-01 22:32:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function abc() {
+	alert("Vui lòng chuyển khoản tới agribank:4205205162442 voi ND: 70kGB + Udid iphone. Sau đó vui lòng chụp bill và liên hệ với tôi qua telegram")
+	window.location = "https://t.me/ioscrackgb"
+}
+
+function abc2() {
+	alert("Vui lòng chuyển khoản tới agribank:4205205162442 voi ND: 150kGB + Udid iphone. Sau đó vui lòng chụp bill và liên hệ với tôi qua telegram")
+	window.location = "https://t.me/ioscrackgb"
+}
+
+function abc3() {
+	alert("Vui lòng chuyển khoản tới agribank:4205205162442 voi ND: 250kGB + Udid iphone. Sau đó vui lòng chụp bill và liên hệ với tôi qua telegram")
+	window.location = "https://t.me/ioscrackgb"
+}
+
+function abc4() {
+	alert("Vui lòng chuyển khoản tới agribank:4205205162442 voi ND: 350kGB + Udid iphone. Sau đó vui lòng chụp bill và liên hệ với tôi qua telegram")
+	window.location = "https://t.me/ioscrackgb"
+}
